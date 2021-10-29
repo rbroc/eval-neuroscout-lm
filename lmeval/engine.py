635b3690b55dacfc7bbde8e41f36a40aa7edcd64
rbroc <rbrrcc@gmail.com>
2021-10-29 10:12:54 -0500

    adapt script to gpt2 only

--- lmeval/engine.py
+++ lmeval/engine.py
@@ -2,6 +2,7 @@ import torch
 from tqdm import tqdm
 import pandas as pd
 import time
+import torch
 from scipy.stats import entropy
 
 
@@ -10,10 +11,8 @@ COLUMNS = ['dataset', 'type', 'model', 'context', 'target',
            'prob_true', 'prob_predicted', 'context_size']
 
 
-class StridingMLM:
-    ''' Class for striding forward LM over a dataset 
-        Applies to BERT-like models, which need explicit masking 
-        of the last token.
+class StridingForwardLM:
+    ''' Class for striding forward LM over a dataset.
     '''
     def __init__(self, 
                  context_length=20):
@@ -22,34 +21,29 @@ class StridingMLM:
         
     def _split(self, whitespaced, tokenizer):
         n_tokens = len(whitespaced)
-        #n_tokens = tokenized['input_ids'].shape[-1]
-        i_start = list(range(0, 
-                             n_tokens-(self.context_length+1)))
-        i_end = [i+(self.context_length+1) for i in i_start]
-        split_tks = [tokenizer(' '.join(whitespaced[i_s:i_e]), return_tensors='pt')['input_ids']
+        i_start = list(range(0, n_tokens-(self.context_length)))
+        i_end = [i+(self.context_length) for i in i_start]
+        split_tks = [tokenizer(' '.join(whitespaced[i_s:i_e]), 
+                               return_tensors='pt')['input_ids']
                      for i_s, i_e in zip(i_start,i_end)]
-        #split_tks = [tokenized['input_ids'][:,i_s:i_e]
-        #             for i_s, i_e in zip(i_start,i_end)]
-        return split_tks
+        targets = [whitespaced[i_e] for i in i_end]
+        return split_tks, targets
     
-    def _preprocess(self, text, tokenizer, gpu):
+    def _preprocess(self, text, tokenizer):
         whitespaced = text.split(' ')
-        #tokenized = tokenizer(text, return_tensors='pt').to(device=f'cuda:{str(gpu)}')
-        # tokenized text is split into n-length windows
-        tokenized_lst = self._split(whitespaced, tokenizer)
-        return tokenized_lst
+        tokenized_lst, targets = self._split(whitespaced, tokenizer)
+        return tokenized_lst, targets
     
-    def _mask(self, list_item, tokenizer):
-        input_ids = list_item.clone() # 
+    def _mask(self, list_item, tokenizer, true):
+        input_ids = list_item.clone()
+        ctx = tokenizer.decode(input_ids[0])
         target_ids = input_ids.clone()
-        #input_ids[0][-1] = tokenizer.mask_token_id
-        input_ids[0][-2] = tokenizer.mask_token_id
-        target_ids[:,:-2] = -100 # replaced -1 with -2
-        target_ids[:,-1] = -100 # added
-        ctx = tokenizer.decode(input_ids[0][:-2]) # replaced -1 with -2
-        wd_id = target_ids[0][-2] # replaced -1 with -2
-        wd = tokenizer.decode(wd_id)
-        return input_ids, target_ids, ctx, wd, wd_id
+        target_ids[:,:] = -100
+        true_id = tokenizer.encode(true, return_tensors='pt')[:,:0] # get id for true token
+        true_token = tokenizer.decode(true_id[0,0]).strip(' ') # get true token model can predict
+        input_ids = torch.cat((input_ids, true_id), axis=-1) # append true token to the inputs
+        target_ids = torch.cat((target_ids, true_id), axis=-1) # append to labels too
+        return input_ids, target_ids, ctx, true_token, true_id[0,0]
     
     def _compute_metrics(self, outputs, wd_id, tokenizer):
         loss = float(outputs.loss.cpu().detach().numpy())
@@ -67,14 +61,15 @@ class StridingMLM:
             model_name, gpu=0):
         time.sleep(.5)
         results = []
-        tokenized_lst = self._preprocess(dataset.text, 
-                                         tokenizer, gpu) # masking
+        tokenized_lst, targets = self._preprocess(dataset.text, 
+                                                  tokenizer) # masking
         print(f'Running {model_name}, '
               f'{dataset.name}, {self.context_length}, '
               f'{len(tokenized_lst)}')
         for i in tqdm(range(len(tokenized_lst))):
             input_ids, target_ids, ctx, wd, wd_id = self._mask(tokenized_lst[i].to(device=f'cuda:{str(gpu)}'), # edited
-                                                               tokenizer) # make masking
+                                                               tokenizer, 
+                                                               targets[i]) # make masking
             outputs = model(input_ids, labels=target_ids)
             metrics = self._compute_metrics(outputs, wd_id, tokenizer)
             results.append((dataset.name, 
@@ -84,25 +79,3 @@ class StridingMLM:
                             self.context_length))
         output = pd.DataFrame(results, columns=COLUMNS)
         return output
-    
-    
-class StridingForwardLM(StridingMLM):
-    ''' Class for striding forward LM over a dataset 
-        Applies to GPT-like models.
-    '''
-    def __init__(self, 
-                 context_length=20):
-        super().__init__(context_length=context_length)
-    
-    def _mask(self, list_item, tokenizer):
-        input_ids = list_item[:,:-1].clone()
-        target_ids = input_ids.clone()
-        target_ids[:,:-1] = -100
-        ctx = tokenizer.decode(input_ids[0])
-        wd_id = list_item[0,-1]
-        wd = tokenizer.decode(wd_id)
-        return input_ids, target_ids, ctx, wd, wd_id
-    
-
-
-    
